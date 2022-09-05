bf54ec6aa6d047bdea4d1644776fbf21da965928
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-09-05 14:19:51 -0700

    change model name from image to photo and migratei it

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Meme, Comment, Image
+from .models import Meme, Comment, Photo
 
 # Register your models here.
 admin.site.register(Meme)
 admin.site.register(Comment)
-admin.site.register(Image)
+admin.site.register(Photo)
--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
-from .models import Meme
+from .models import Meme, Photo
+import uuid
+import boto3
+
+S3_BASE_URL = 'https://s3.us-east-1.amazonaws.com/'
+BUCKET ='beastcoastmeme'
 
 
 # Create your views here.
@@ -37,3 +42,18 @@ def signup(request):
 class MemeCreate(CreateView):
   model = Meme
   fields = ['title', 'caption',]
+
+
+
+def add_photo(request, meme_id):
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+    s3 = boto3.client('s3')
+    key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+    try:
+      s3.upload_fileobj(photo_file, BUCKET, key)
+      url = f"{S3_BASE_URL}{BUCKET}/{key}"
+      Photo.objects.create(url=url, meme_id=meme_id)
+    except:
+      print('An error occured uploading file to S3')
+  return redirect('detail', meme_id=meme_id)
