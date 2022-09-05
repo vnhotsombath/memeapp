c84f9054403e1fb204b6a917ad33a06bdf6f0c04
username <youremail@domain.com>
2022-09-05 19:40:06 -0400

    add user index

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
     path('memes/create/', views.MemeCreate.as_view(), name='memes_create'),
-    path('memes/<int:meme_id>/add_photo/', views.add_photo, name='add_photo'),]
+    path('memes/<int:meme_id>/add_photo/', views.add_photo, name='add_photo'),
+    path('memes/', views.memes_index, name='memes'),
+    ]
